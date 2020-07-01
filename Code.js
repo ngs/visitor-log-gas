07f9dc7bc64e9b641de866d76b96b2b424b5a219
Atsushi Nagase <a@ngs.io>
2020-07-01 23:12:18 +0900

    Format code

--- Code.js
+++ Code.js
@@ -9,13 +9,15 @@ function onSubmit(e) {
   values[0][8] = id;
   row.setValues(values);
   Logger.log('ID: ' + id + ' Timestamp: ' + timestamp);
-  var blocks = [{
-    type: 'section',
-    text: {
-      type: 'mrkdwn',
-      text: 'New visitor was recorded'
-    }
-  }];
+  var blocks = [
+    {
+      type: 'section',
+      text: {
+        type: 'mrkdwn',
+        text: 'New visitor was recorded',
+      },
+    },
+  ];
   var fields = [];
   for (var i = 0; i < itemResponses.length; i++) {
     var value = itemResponses[i].getResponse();
@@ -23,52 +25,58 @@ function onSubmit(e) {
     if (title) {
       fields.push({
         type: 'mrkdwn',
-        text: '*' + title + '*\n\t' + value
+        text: '*' + title + '*\n\t' + value,
       });
     }
   }
   blocks.push({
     type: 'section',
-    fields: fields
+    fields: fields,
   });
   blocks.push({
     type: 'actions',
-    elements: [{
-      type: 'button',
-      text: {
-        type: 'plain_text',
-        text: 'Mark as left',
-        emoji: true
+    elements: [
+      {
+        type: 'button',
+        text: {
+          type: 'plain_text',
+          text: 'Mark as left',
+          emoji: true,
+        },
+        url: getWebAppURL() + '?id=' + id,
       },
-      url: getWebAppURL() + '?id=' + id
-    }]
+    ],
   });
-  notifySlack({ blocks: blocks });
+  notifySlack({blocks: blocks});
 }
 
 function notifySlack(payload) {
-  var url = PropertiesService.getScriptProperties().getProperty("SLACK_WEBHOOK_URL");
+  var url = PropertiesService.getScriptProperties().getProperty(
+    'SLACK_WEBHOOK_URL',
+  );
   var options = {
     method: 'post',
-    headers: { 'Content-Type': 'application/json' },
-    payload: JSON.stringify(payload)
+    headers: {'Content-Type': 'application/json'},
+    payload: JSON.stringify(payload),
   };
   var response = UrlFetchApp.fetch(url, options);
   Logger.log(response);
 }
 
 function getForm() {
-  var formId = PropertiesService.getScriptProperties().getProperty("FORM_ID");
+  var formId = PropertiesService.getScriptProperties().getProperty('FORM_ID');
   return FormApp.openById(formId);
 }
 
 function getSheet() {
-  var worksheetId = PropertiesService.getScriptProperties().getProperty("WORKSHEET_ID");
+  var worksheetId = PropertiesService.getScriptProperties().getProperty(
+    'WORKSHEET_ID',
+  );
   return SpreadsheetApp.openById(worksheetId).getSheets()[0];
 }
 
 function getWebAppURL() {
-  return PropertiesService.getScriptProperties().getProperty("WEBAPP_URL");
+  return PropertiesService.getScriptProperties().getProperty('WEBAPP_URL');
 }
 
 function findResponseRow(response) {
@@ -90,19 +98,26 @@ function findResponseRow(response) {
 }
 
 function createNotFoundTemplate() {
-  return HtmlService.createTemplate("<h1>Not Found</h1><p>Response not found for ID <code><?= id ?></code></p>");
+  return HtmlService.createTemplate(
+    '<h1>Not Found</h1><p>Response not found for ID <code><?= id ?></code></p>',
+  );
 }
 
 function createFormTemplate() {
-  var template = HtmlService.createTemplate('<form action="<?= postURL ?>" target="_top" method="POST"><input type="hidden" name="id" value="<?= id ?>"><input type="submit" value="<?= label ?>"></form>');
+  var template = HtmlService.createTemplate(
+    '<form action="<?= postURL ?>" target="_top" method="POST"><input type="hidden" name="id" value="<?= id ?>"><input type="submit" value="<?= label ?>"></form>',
+  );
   template.postURL = getWebAppURL();
   return template;
 }
 
 function addMetaTags(output) {
   return output
-  .addMetaTag("viewport", "width=device-width, initial-scale=1, maximum-scale=1")
-  .setTitle("instance0 visitor log");
+    .addMetaTag(
+      'viewport',
+      'width=device-width, initial-scale=1, maximum-scale=1',
+    )
+    .setTitle('instance0 visitor log');
 }
 
 function doGet(e) {
@@ -111,7 +126,7 @@ function doGet(e) {
   var response;
   try {
     response = form.getResponse(id);
-  } catch(err) {
+  } catch (err) {
     Logger.log(err);
   }
   var row = findResponseRow(response);
@@ -122,9 +137,9 @@ function doGet(e) {
     template = createFormTemplate();
     var values = row.getValues();
     if (values[0][7]) {
-      template.label = "Update";
+      template.label = 'Update';
     } else {
-      template.label = "Mark as left";
+      template.label = 'Mark as left';
     }
   }
   template.id = id;
@@ -137,7 +152,7 @@ function doPost(e) {
   var response;
   try {
     response = form.getResponse(id);
-  } catch(err) {
+  } catch (err) {
     Logger.log(err);
   }
   var row = findResponseRow(response);
@@ -146,7 +161,7 @@ function doPost(e) {
     template = createNotFoundTemplate();
   } else {
     template = createFormTemplate();
-    template.label = "Update";
+    template.label = 'Update';
     var values = row.getValues();
     values[0][7] = new Date();
     row.setValues(values);
